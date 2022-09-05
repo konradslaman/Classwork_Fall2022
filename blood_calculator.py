5ebe1fbe7dd5eb5de2768806bd637eb9752b4844
Konrad Slaman <konrad.slaman@duke.edu>
2022-09-05 13:00:00 -0400

    Added the HDL testing functionality to the interface

--- blood_calculator.py
+++ blood_calculator.py
@@ -1,13 +1,17 @@
 def interface():
     print("Blood Calculator")
     print("Options:")
+    print("1 - HDL Test")
     print("9 - Quit")
+
 
     keep_Running = True
     while(keep_Running):
         choice = input("Enter choice: ")
         if choice == "9":
             return
+        if choice == "1":
+            drive_HDL()
 
 def keyboardInputHDL():
     return int(input("Enter HDL Value:"))
