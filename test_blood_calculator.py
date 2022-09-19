051e28849844ac385d58c948acf0ca640180896b
Konrad Slaman <konrad.slaman@duke.edu>
2022-09-19 12:55:10 -0400

    Created a testing function for the HDL that takes in parameters

---
+++ test_blood_calculator.py
@@ -0,0 +1,28 @@
+import pytest
+
+@pytest.mark.parametrize("HDL, level" ,
+    [(85, "Normal"),
+    (50, "Borderline Low"),
+    (30, "Low")])
+
+def test_check_HDL(HDL, level):
+    from blood_calculator import check_HDL
+    answer = check_HDL(HDL)
+    expected = level
+    assert answer == expected
+
+
+
+"""
+def test_check_HDL_BL():
+    from blood_calculator import check_HDL
+    answer = check_HDL(50)
+    expected = "Borderline Low"
+    assert answer == expected
+
+def test_check_HDL_Low():
+    from blood_calculator import check_HDL
+    answer = check_HDL(30)
+    expected = "Low"
+    assert answer == 
+"""
